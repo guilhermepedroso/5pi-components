d98b3d0f7b1d9f8d3728f7d013c39d50b9d65e7c
Guilherme Pedroso <guimeop@gmail.com>
2025-07-30 15:00:20 -0300

    Initialize Vite project with basic structure, including HTML, CSS, and JavaScript files. Added .gitignore for node_modules, bun.lock for dependency management, and SVG assets for branding. Implemented a counter functionality and integrated Swiper for UI components.

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import "./style.css";
+
+new Swiper(".swiper-day-trade", {
+	slidesPerView: 1,
+	spaceBetween: 24,
+	breakpoints: {
+		640: {
+			slidesPerView: 2,
+		},
+		1024: {
+			slidesPerView: 4,
+		},
+	},
+	grabCursor: true,
+	navigation: {
+		nextEl: ".swiper-day-trade-button-next",
+		prevEl: ".swiper-day-trade-button-prev",
+	},
+});
+
+new Swiper(".swiper-shorts-cases", {
+	slidesPerView: "auto",
+	grabCursor: true,
+	spaceBetween: 24,
+	navigation: {
+		nextEl: ".swiper-shorts-cases-button-next",
+		prevEl: ".swiper-shorts-cases-button-prev",
+	},
+});
