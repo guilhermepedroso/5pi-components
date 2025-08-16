35f399512106710645e96dd6e57629f368b7fe6e
Guilherme Pedroso <guimeop@gmail.com>
2025-08-16 10:45:50 -0300

    feat: add clients component with Handlebars template and integrate into main structure, while removing advisors section

---
+++ src/components/clients/clients.js
@@ -0,0 +1,35 @@
+const clientsData = {
+	clients: [
+		{ number: 1 },
+		{ number: 2 },
+		{ number: 3 },
+		{ number: 4 },
+		{ number: 5 },
+		{ number: 6 },
+		{ number: 7 },
+		{ number: 8 },
+		{ number: 9 },
+		{ number: 10 },
+		{ number: 11 },
+		{ number: 12 },
+		{ number: 13 },
+	],
+};
+
+orion.renderTemplate({
+	templateId: "clients-template",
+	containerId: "clients-container",
+	data: clientsData,
+	callback: () => {
+		const clientsSwiperConfig = {
+			slidesPerView: 4,
+			// spaceBetween: 24,
+			autoplay: {
+				delay: 2000,
+				disableOnInteraction: false,
+			},
+			grabCursor: true,
+		};
+		new Swiper(".swiper-clients", clientsSwiperConfig);
+	},
+});
