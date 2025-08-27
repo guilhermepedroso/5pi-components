dd710bdd5f8dcf295f5b4b47539ccda5bfc25be5
Guilherme Pedroso <guimeop@gmail.com>
2025-08-27 08:13:56 -0300

    feat: add video modal component and hero section with play button, remove deprecated play.html file, and update styles for improved user experience

---
+++ src/components/hero/hero.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+	const videoPlayButton = document.querySelector(".video-play-hero-button");
+	videoPlayButton.addEventListener("click", () => {
+		window.orion.videoModal.open("dQw4w9WgXcQ");
+	});
+});
