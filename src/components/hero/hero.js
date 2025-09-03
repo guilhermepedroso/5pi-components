43fa712376cce32bf05dd2106ac559d6a043cd45
Guilherme Pedroso <guimeop@gmail.com>
2025-09-03 09:28:56 -0300

    feat: enhance hero section with new background image, updated styles, and dynamic text toggle for improved user engagement

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -3,4 +3,9 @@ window.addEventListener("DOMContentLoaded", () => {
 	videoPlayButton.addEventListener("click", () => {
 		window.orion.videoModal.open("dQw4w9WgXcQ");
 	});
+
+	setInterval(() => {
+		document.querySelector("#hero-title .text-1").classList.toggle("hidden");
+		document.querySelector("#hero-title .text-2").classList.toggle("hidden");
+	}, 6000);
 });
